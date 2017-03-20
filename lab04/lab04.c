c5158d7d0c7e6762098f69cee4531acd522feb0a
Pedro Bastos <pedro.bbastos123@gmail.com>
2017-03-20 14:44:13 -0300

    Aprimoramento Lab 04

--- lab04/lab04.c
+++ lab04/lab04.c
@@ -5,7 +5,6 @@ int main()
 	int pesos [4];
 	int soma = 0;
 	int indiceMaisPesado = 0;
-	int maisPesado = 0;
 
 	scanf("%d %d %d %d", &pesos[0], &pesos[1], &pesos[2], &pesos[3]);
 
@@ -13,16 +12,13 @@ int main()
 	{
 		soma += pesos[i];
 
-		if(pesos[i] > maisPesado)
-		{
-			maisPesado = pesos[i];
+		if(pesos[i] > pesos[indiceMaisPesado])
 			indiceMaisPesado = i;
-		}
 	}
 
-	if (soma % 2 == 0 && maisPesado <= soma/2)
+	if (soma % 2 == 0 && pesos[indiceMaisPesado] <= soma/2)
 	{
-		if(maisPesado == soma/2)
+		if (pesos[indiceMaisPesado] == soma/2)
 		{
 			printf("sim\n");
 			return 0; //eh possivel
@@ -30,15 +26,16 @@ int main()
 		else
 		{
 			for (int i = 0; i < 4; i++)
-				for (int j = 0; j < 4; j++)
+			{
+				if (i != indiceMaisPesado)
 				{
-					if (i == indiceMaisPesado && i != j && maisPesado + pesos[j] == soma/2)
+					if (pesos[indiceMaisPesado] + pesos[i] == soma/2)
 					{
-						//eh possivel
 						printf("sim\n");
-						return 0;
+	                                        return 0;
 					}
-				}
+				}	
+			}
 			
 			printf("nao\n");
 			return 0; //naum eh possivel
