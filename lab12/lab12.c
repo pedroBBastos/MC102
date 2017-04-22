096ce328d56344749673afcc0a6b21e60699c2c5
pedro <pedro.bbastos123@gmail.com>
2017-04-22 01:28:35 -0300

    Continuação Lab12

--- lab12/lab12.c
+++ lab12/lab12.c
@@ -34,11 +34,33 @@ void deletarPalavra(char palavra_deletar[], char texto[])
 
             if(strcmp(palavraAtual, palavra_deletar) == 0)
             {
+                int fimPalavraAtual;
+
+                if(texto[i] == '\n') // se o caracter atual for '\n' atualizar o fim da palavra
+                                     // para nao acabar apagar o \n
+                    fimPalavraAtual = i-1;
+                else
+                    fimPalavraAtual = i;
+
+                for(int k=fimPalavraAtual; k>=inicioPalavraAtual; k--)
+                {
+                    int indiceDesloc = k;
+                    while(texto[indiceDesloc] != '\0') // ate o fim do texto
+                    {
+                        texto[indiceDesloc] = texto[indiceDesloc+1];
+                        indiceDesloc++;
+                    }
+                }
+
+                i -= inicioPalavraAtual; //atualizar i pq o vetor inteiro foi deslocado e i deve voltar
+                                         //no caracter em que estava
             }
             
             inicioPalavraAtual = i+1;
         }
     }
+
+    printf("%s", texto);
 }
 
 void inverterPalavra(char palavra_inverter[], char texto[])
@@ -55,12 +77,11 @@ void inverterPalavra(char palavra_inverter[], char texto[])
             char palavraAtual[50];
             criaPalavra(texto, inicioPalavraAtual, palavraAtual, i-inicioPalavraAtual);
 
-            printf("%s\n", palavraAtual);
-
             if(strcmp(palavraAtual, palavra_inverter) == 0)
             {
-                int metade = (i-inicioPalavraAtual)/2;
                 int p = i-1;
+                int metade = (p+inicioPalavraAtual)/2;
+
                 for(int k=inicioPalavraAtual; k<=metade; k++, p--)
                 {
                     char aux = texto[p];
@@ -72,6 +93,8 @@ void inverterPalavra(char palavra_inverter[], char texto[])
             inicioPalavraAtual = i+1;
         }
     }
+
+    printf("%s", texto);
 }
 
 void trocarPalavra(char palavra_velha[], char palavra_nova[], char texto[])
@@ -90,11 +113,30 @@ void trocarPalavra(char palavra_velha[], char palavra_nova[], char texto[])
 
             if(strcmp(palavraAtual, palavra_velha) == 0)
             {
+                int tamanhoPalavraVelha = strlen(palavra_velha);
+                int tamanhoPalavraNova = strlen(palavra_nova);
+
+                // tamanhoVetor += tamanhoPalavraNova - tamanhoPalavraVelha
+                if(tamanhoPalavraVelha < tamanhoPalavraNova)
+                {
+                    //vetor sera deslocado para frente
+                }
+                else
+                    if(tamanhoPalavraVelha > tamanhoPalavraNova)
+                    {
+                        //vetor sera deslocado para tras
+                    }
+                    else
+                    {
+                        //basta substituir, pois possuem o mesmo tamanho
+                    }
             }
 
             inicioPalavraAtual = i+1;
         }
     }
+
+    printf("%s", texto);
 }
 
 int main()
@@ -102,21 +144,39 @@ int main()
     char texto[1000];
 
     fgets(texto, 1000, stdin);
+    printf("%s", texto);
 
-    char operacao;
+    char operacao[3]; //caracter de operacao, \n e \0
     for(;;)
     {
-        scanf("%c", &operacao);
+        //scanf("%c", &operacao);
+        fgets(operacao, 3, stdin);
 
-        if(operacao == 'Q')
+        //depois de ler um caracter, ja vai cai nos codigos abaixo.
+        //Assim, quando der enter (\n) para digitar os parametros
+        //o fgets para os parametro so vai pegar o \n, chamando a
+        //respectiva funcao sem os devidos paramentros.
+
+        //if(strcmp(operacao, "Q") == 0) -> o certo seria strcmp(operacao, "Q\n")
+        if(operacao[0] == 'Q')
             break;
 
-        switch(operacao)
+        switch(operacao[0])
         {
             case 'D' : 
             {
                 char palavra_deletar[50];
                 fgets(palavra_deletar, 50, stdin);
+
+                //removendo '\n' da string lida:
+                //a funcao strchr procura pela primeira ocorrencia de determinado caracter
+                //em uma dada string, e retorna o ponteiro para a posicao daquele caracter
+                //
+                //outro jeito de fazer eh remover o /n na mao -> vendo tamanho da string
+                //e acessando tam-2 = '\0'
+                char* ponteiroBarraN;
+                if ((ponteiroBarraN=strchr(palavra_deletar, '\n')) != NULL)
+                    *ponteiroBarraN = '\0';
 
                 deletarPalavra(palavra_deletar, texto);
                 break;
@@ -126,6 +186,20 @@ int main()
             {
                 char palavra_inverter[50];
                 fgets(palavra_inverter, 50, stdin);
+
+                //aqui tbem acontece bosta
+                //com o fgets, acaba armazenando o \n, o que
+                //na hora da comparacao da merda (por conter o \n em uma string e nao na outra).
+
+                //removendo '\n' da string lida:
+                //a funcao strchr procura pela primeira ocorrencia de determinado caracter
+                //em uma dada string, e retorna o ponteiro para a posicao daquele caracter
+                //
+                //outro jeito de fazer eh remover o /n na mao -> vendo tamanho da string
+                //e acessando tam-2 = '\0'
+                char* ponteiroBarraN;
+                if ((ponteiroBarraN=strchr(palavra_inverter, '\n')) != NULL)
+                    *ponteiroBarraN = '\0';
 
                 inverterPalavra(palavra_inverter, texto); 
                 break;
@@ -137,11 +211,18 @@ int main()
                 fgets(palavra_velha, 50, stdin);
                 fgets(palavra_nova, 50, stdin);
 
+                char* ponteiroBarraN;
+                if ((ponteiroBarraN=strchr(palavra_velha, '\n')) != NULL)
+                    *ponteiroBarraN = '\0';
+
+                if ((ponteiroBarraN=strchr(palavra_nova, '\n')) != NULL)
+                    *ponteiroBarraN = '\0';
+
                 trocarPalavra(palavra_velha, palavra_nova, texto); 
                 break;
             }
         }
     }
 
-    printf("%s", texto);
+    //printf("%s", texto);
 }
