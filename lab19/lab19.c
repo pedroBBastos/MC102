407cbdc151855ae8135b17563e01169be2e24478
Pedro Bastos <pedro.bbastos123@gmail.com>
2017-06-14 17:37:16 -0300

    Continuação Lab19.

--- lab19/lab19.c
+++ lab19/lab19.c
@@ -20,8 +20,63 @@
  * anagrama.
  */
 void generate(char *string_ordenada, int *letra_usada, char *word, int n, int k){
+
+  printf("----------\n");
+  printf("n -> %d\n", n);
+  printf("k -> %d\n", k);
+  printf("----------\n");
+
+  for(int i=0; i<k; i++)
+    printf("%c", word[i]);
+  printf("\n----------\n");
+
+  for(int i=0; i<(n+k); i++)
+    printf("%d, ", letra_usada[i]);
+  printf("\n----------\n");
   
-  if(n == k) // condicao de parada?
+  if(n == 0)
   {
+    word[k] = '\0';
+    printf("Anagrama ->> %s\n", word); // printando o anagrama
+
+    return;
   }
+  
+  for(int i=0; i<(n+k); i++) // aqui deve-se percorrer para as n letras disponíveis para tanto
+  {
+    // verificar se a mesma letra ja foi usada para chamadas recursivas
+    //percorrer o vetor e ver se a letra atual eh igual a alguma letra ja usada
+    
+    if(!letra_usada[i])
+    {
+      int repetida = 0;
+      for(int j=0; j<(n+k); j++)
+	if(letra_usada[j] == 1 && letra_usada[i] == letra_usada[j])
+	{
+	  repetida = 1;
+	  break;
+	}
+
+      if(!repetida)
+      {
+	word[k] = string_ordenada[i];
+	letra_usada[i] = 1;
+	generate(string_ordenada, letra_usada, word, n-1, k+1);
+	//letra_usada[i]
+      }    
+    }
+  }
+
+  //zerar os usados menos o atual aqui?? - parece que sim - acaba uma chamada recursiva
+  for(int i=k; i<(n+k); i++)
+  {
+    printf("Limpando posicao %d\n", i);
+    letra_usada[i] = 0;
+  }
+
+  for(int i=0; i<(n+k); i++)
+    printf("%d, ", letra_usada[i]);
+  printf("\n");
+
+  return;
 }
