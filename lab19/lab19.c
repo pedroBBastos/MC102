9565f7ece2744ad410dcbd854a6c10efae7e23a1
Pedro Bastos <pedro.bbastos123@gmail.com>
2017-06-17 18:03:13 -0300

    Término Lab 19.

--- lab19/lab19.c
+++ lab19/lab19.c
@@ -3,6 +3,12 @@
 #include <string.h>
 
 /*
+  Este programa tem por objetivo gerar todos os anagramas de uma string dada recursivamente.
+
+  Nome : Pedro Barros Bastos   RA : 204481
+ */
+
+/*
  * Entrada: 
  *          string_ordenada: a string original, que devemos processar 
  *                           para encontrarmos todos os anagramas
@@ -21,62 +27,31 @@
  */
 void generate(char *string_ordenada, int *letra_usada, char *word, int n, int k){
 
-  printf("----------\n");
-  printf("n -> %d\n", n);
-  printf("k -> %d\n", k);
-  printf("----------\n");
-
-  for(int i=0; i<k; i++)
-    printf("%c", word[i]);
-  printf("\n----------\n");
-
-  for(int i=0; i<(n+k); i++)
-    printf("%d, ", letra_usada[i]);
-  printf("\n----------\n");
-  
+  //condicao de parada -> se nao ha mais letras para colocar no anagrama atual, printa-lo
   if(n == 0)
   {
     word[k] = '\0';
-    printf("Anagrama ->> %s\n", word); // printando o anagrama
+    printf("%s\n", word); // printando o anagrama
 
     return;
   }
-  
-  for(int i=0; i<(n+k); i++) // aqui deve-se percorrer para as n letras disponíveis para tanto
-  {
-    // verificar se a mesma letra ja foi usada para chamadas recursivas
-    //percorrer o vetor e ver se a letra atual eh igual a alguma letra ja usada
-    
-    if(!letra_usada[i])
-    {
-      int repetida = 0;
-      for(int j=0; j<(n+k); j++)
-	if(letra_usada[j] == 1 && letra_usada[i] == letra_usada[j])
-	{
-	  repetida = 1;
-	  break;
-	}
 
-      if(!repetida)
-      {
-	word[k] = string_ordenada[i];
-	letra_usada[i] = 1;
-	generate(string_ordenada, letra_usada, word, n-1, k+1);
-	//letra_usada[i]
-      }    
+  //se nao, percorrer todas as letras que ainda nao foram usadas e fazer a recursao
+  for(int i=0; i<(n+k); i++)
+  {
+    
+    if(!letra_usada[i] && word[k] != string_ordenada[i]) // assim ja verifica se ainda nao foi usada e se um caracter igual ja foi
+                                                         // usado na mesma chamada, sem precisar percorrer toda a string de novo
+                                                         // -> letra_usada serve mais para as chamadas subsequentes
+    {
+      word[k] = string_ordenada[i];
+      letra_usada[i] = 1;
+      generate(string_ordenada, letra_usada, word, n-1, k+1);
+      letra_usada[i] = 0;
     }
   }
 
-  //zerar os usados menos o atual aqui?? - parece que sim - acaba uma chamada recursiva
-  for(int i=k; i<(n+k); i++)
-  {
-    printf("Limpando posicao %d\n", i);
-    letra_usada[i] = 0;
-  }
-
-  for(int i=0; i<(n+k); i++)
-    printf("%d, ", letra_usada[i]);
-  printf("\n");
+  word[k] = '\0';
 
   return;
 }
