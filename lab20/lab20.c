43cb8d75a5ac8435e54198110f773c12732bf0d2
Pedro Bastos <pedro.bbastos123@gmail.com>
2017-06-24 19:37:51 -0300

    Finalização Lab20.

--- lab20/lab20.c
+++ lab20/lab20.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
+/*
+    Este programa tem por objetivo determinar quantas bases do imperio e quantas
+  bases rebeldes estão presentes em uma região a partir do mapa de um setor de uma região
+  que será dado como entrada.
+    A saída informa quantas bases rebeldes e quantas bases do imperio estão na dada região.
+    As bases rebeldes sao identificadas pelo caracter '#' e as bases do imperio pelo caracter
+  '*'. O caracter '-' identifica espaço vazio.
+
+    Nome : Pedro Barros Bastos    RA : 204481
+
+*/
+
 #define MAX 102
 
+//metodo que recursivamente marca as posicoes da matriz que sao de determinado grupo
 void efetuarMarcacao(char map[MAX][MAX], char auxMap[MAX][MAX], char c, int i, int j);
 
 int main()
@@ -15,8 +28,6 @@ int main()
   //poderemos ter uma matriz auxiliar que marca as posicoes ja verificadas da base atual.
   //assim, na primeira passagem, verificamos se ela ja nao foi verificada, a partir desta matriz auxiliar.
   //se naum, iniciar o processo recursivo para verificação/marcação.
-  //Pensar em um modo para que, no retorno de todo o processo de recursao que verificou uma base, a contagem se
-  //efetue -> soh contar mais um depois da chamada da funcao
 
   //para criação de matriz aumentada e facilitar o trabalho
   rows += 2;
@@ -25,40 +36,23 @@ int main()
   for(int i = 0; i < rows; i++)
     for(int j = 0; j < cols; j++)
     {
-      
-      if((i == 0 || i == MAX-1) || (j == 0 || j == MAX-1)) // preenchimento das bordas
-          map[i][j] = '-';
+
+      if((i == 0 || i == rows-1) || (j == 0 || j == cols-1)) // cantos da matriz aumentada
+        map[i][j] = '-';
       else
-        scanf("%c ", &map[i][j]);
+      {
+        char c;
+
+        do
+        {
+          scanf("%c", &c);
+        } while(c == ' ' || c == '\n' || c == '\0'); //dispensando caracteres inuteis vindos da entrada
+
+        map[i][j] = c;
+      }
 
       auxMap[i][j] = 'n';
     }
-
-  /*
-  printf("\n Printando o lido \n\n");
-
-  for(int i = 0; i < rows; i++)
-  {
-    for(int j = 0; j < cols; j++)
-    {
-      printf("%c ", map[i][j]);
-    }
-
-    printf("\n");
-  }
-
-  printf("\n\n");
-
-  for(int i = 0; i < rows; i++)
-  {
-    for(int j = 0; j < cols; j++)
-    {
-      printf("%c ", auxMap[i][j]);
-    }
-
-    printf("\n");
-  }
-  */
 
   for(int i = 1; i < rows-1; i++)
     for(int j = 1; j < cols-1; j++)
@@ -67,6 +61,8 @@ int main()
       {
         switch(map[i][j])
         {
+          // efetuar a chamada recursiva correspondente para marcar na auxMap todas as
+          //posicoes da base atual.
           case '*' : basesImperio++; efetuarMarcacao(map, auxMap, '*', i, j); break;
           case '#' : basesRebeldes++; efetuarMarcacao(map, auxMap, '#', i, j); break;
         }
@@ -83,6 +79,7 @@ void efetuarMarcacao(char map[MAX][MAX], char auxMap[MAX][MAX], char c, int i, i
   {
     auxMap[i][j] = '!';
 
+    //verificacao das posicoes vizinhas
     efetuarMarcacao(map, auxMap, c, i-1, j-1);
     efetuarMarcacao(map, auxMap, c, i-1, j);
     efetuarMarcacao(map, auxMap, c, i-1, j+1);
